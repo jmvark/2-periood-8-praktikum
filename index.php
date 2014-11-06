82dde650adaf2c65d6185e219203228b8ad2f256
JasonMax_2 <jason-max.vark@khk.ee>
2014-11-06 11:44:08 +0200

    final commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title>2-periood-8-praktikum</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<?php
+		$name = "Test";
+		$value = 45;
+		$expire = "";
+		setcookie($name, $value, $expire);
+
+	?>
+
+</body>
+</html>
